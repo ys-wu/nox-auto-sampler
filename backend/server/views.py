61b44623e44d53bfc545f64d9d64c8873033eb20
Yusheng Wu <wuyusheng1023@gmail.com>
2021-02-23 23:23:04 +0200

    browser gets local setting JSON file from Django

--- backend/server/views.py
+++ backend/server/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.views import View
 from rest_framework import viewsets
 
-# import json
+import json
 
 from server.models import Series
 from server.serializers import SeriesSerializer
@@ -18,9 +18,9 @@ class SeriesViewSet(viewsets.ModelViewSet):
 
 class Setting(View):
   def get(self, request):
-    # with open(setting_url) as data_file:
-    #   data = json.load(data_file)
-    return JsonResponse({"hello":1})
+    with open(setting_url) as data_file:
+      data = json.load(data_file)
+      return JsonResponse(data, safe=False)
 
 #   def post(self, request):
 #     return JsonResponse({"hello": 1}
