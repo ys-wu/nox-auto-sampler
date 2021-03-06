82aa21096a65dd7576aed27e8b29f3afcd3d1248
Yusheng Wu <wuyusheng1023@gmail.com>
2021-03-06 10:51:50 +0200

    RawRecordForm collect all data

--- nginx/frontend/src/components/RawRecordForm.jsx
+++ nginx/frontend/src/components/RawRecordForm.jsx
@@ -54,6 +54,72 @@ export default function RawRecordForm({ start, setting, data }) {
     'noxCoef'
   ];
 
+  const onChangeName = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['name'] = value;
+    setState(newState);
+  };
+
+  const onSelectProjectName = value => {
+    const newState = { ...state };
+    newState['projectName'] = value;
+    setState(newState);
+  };
+
+  const onSelectMethod = value => {
+    const newState = { ...state };
+    newState['method'] = value;
+    setState(newState);
+  };
+
+  const onSelectInstrumentName = value => {
+    const newState = { ...state };
+    newState['instrumentName'] = value;
+    setState(newState);
+  };
+
+  const onSelectAssetNumber = value => {
+    const newState = { ...state };
+    newState['assetNumber'] = value;
+    setState(newState);
+  };
+
+  const onChangeBalanceFlow = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['balanceFlow'] = value;
+    setState(newState);
+  };
+
+  const onChangeAmbTemp = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['ambTemp'] = value;
+    setState(newState);
+  };
+
+  const onChangeAmbRh = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['ambRh'] = value;
+    setState(newState);
+  };
+
+  const onChangeAmbPress = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['ambPress'] = value;
+    setState(newState);
+  };
+
+  const onChangeStv = e => {
+    const value = e.target.value;
+    const newState = { ...state };
+    newState['stv'] = value;
+    setState(newState);
+  };
+
   const onChangeRange = e => {
     const value = e.target.value;
     const newState = { ...state };
@@ -132,53 +198,57 @@ export default function RawRecordForm({ start, setting, data }) {
       name="control-hooks" 
       onFinish={onFinish}
     >
+      <Form.Item name="name" label="序列名称">
+        <Input placeholder="请输入分析序列唯一标识" onChange={onChangeName} />
+      </Form.Item>
+
       <Form.Item name="projectName" label="项目名称">
-        <Select placeholder={"项目名称"}>
+        <Select placeholder={"项目名称"} onSelect={onSelectProjectName}>
           {setting['projectName'].map((item, index) => <Option key={index} value={item}>{item}</Option>)}
         </Select>
       </Form.Item>
 
       <Form.Item name="method" label="分析方法">
-        <Select placeholder={"分析方法"}>
+        <Select placeholder={"分析方法"} onSelect={onSelectMethod}>
           {setting['method'].map((item, index) => <Option key={index} value={item}>{item}</Option>)}
         </Select>
       </Form.Item>
 
       <Form.Item name="instrumentName" label="仪器名称及型号">
-        <Select placeholder={"仪器名称及型号"}>
+        <Select placeholder={"仪器名称及型号"} onSelect={onSelectInstrumentName}>
           {setting['instrumentName'].map((item, index) => <Option key={index} value={item}>{item}</Option>)}
         </Select>
       </Form.Item>
       
       <Form.Item name="assetNumber" label="固定资产登记号">
-        <Select placeholder={"固定资产登记号"}>
+        <Select placeholder={"固定资产登记号"} onSelect={onSelectAssetNumber}>
           {setting['assetNumber'].map((item, index) => <Option key={index} value={item}>{item}</Option>)}
         </Select>
       </Form.Item>
 
       <Form.Item name="balanceFlow" label="平衡气流">
-        <Input placeholder="平衡气流 (ml/min)" />
+        <Input placeholder="平衡气流 (ml/min)" onChange={onChangeBalanceFlow} />
       </Form.Item>
 
       <Form.Item name="ambTemp" label="室温">
-        <Input placeholder="室温 (摄氏度)" />
+        <Input placeholder="室温 (摄氏度)" onChange={onChangeAmbTemp} />
       </Form.Item>
 
       <Form.Item name="ambRh" label="相对湿度">
-        <Input placeholder="相对湿度 (%)" />
+        <Input placeholder="相对湿度 (%)" onChange={onChangeAmbRh} />
       </Form.Item>
 
       <Form.Item name="ambPress" label="大气压">
-        <Input placeholder="大气压 (Mpa)" />
+        <Input placeholder="大气压 (Mpa)" onChange={onChangeAmbPress} />
       </Form.Item>
 
       <Form.Item name="stv" label="STV">
-        <Input placeholder="数据稳定判断值" />
+        <Input placeholder="数据稳定判断值" onChange={onChangeStv} />
       </Form.Item>
 
-      <Form.Item name="onChangeRange" label="量程">
+      <Form.Item name="noxRange" label="量程">
         <Input
-          placeholder="量程 (PPM)"
+          placeholder={state['noxRange'] ? state['noxRange'] : "量程 (PPM)"}
           defaultValue={state['noxRange']}
           onChange={onChangeRange}
         />
